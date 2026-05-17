9e927ecf0a37f9fe56ecad34bbcaa9e854680be6
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-17 14:33:58 +0500

    use effect snippits

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function App() {
 
+useEffect(() => {
+  first
 
-  
+  return () => {
+    second
+  }
+}, [third])
+
+
 
   return (
     <div>
