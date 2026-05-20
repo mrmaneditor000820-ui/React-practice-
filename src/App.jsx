f6b1bd0111f48953c142a7f22668e8e495d7bdb4
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-20 06:31:30 +0500

    coding

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 function App() {
-  const [age, setAge] = useState(0)
-  const [doubleage, setdoubleAge] = useState(0)
-
-useEffect(() => {
-  
-
-console.log("useEffect")
-}, [age])
-
-
-
   return (
-    <div>
-      
-    </div>
+
+  
   )
 }
 
